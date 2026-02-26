19be32bc44d41e11d6d054570e2edb3d45dea99a
Aiman Siddiqua <siddiquaaimen@gmail.com>
2026-02-26 23:19:07 +0500

    calculate age

---
+++ age.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    float age;
+    float days;
+    cout<<"Enter your age in years:";
+    cin>>age;
+    days = age * 365;
+    cout <<"your age in days is approximately "<<days<<" days";
+    return 0;
+
+
+    
+}

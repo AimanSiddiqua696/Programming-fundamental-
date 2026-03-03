9917feabd104270abcc87663795215b2a50af2ee
Aiman Siddiqua <siddiquaaimen@gmail.com>
2026-03-03 13:18:28 +0500

    Largest number

---
+++ Conditionals.cpp/Largest.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1;
+    int num2;
+    cout<<"Enter number 1: ";
+    cin>>num1;
+    cout<<"Enter number 2: ";
+    cin>>num2;
+    if(num1 > num2){
+        cout<<"number 1 is greater than numbber 2.";
+    }
+        else{
+             cout<<"number 2 is greater than numbber 1.";
+
+        }
+        return 0;
+    }

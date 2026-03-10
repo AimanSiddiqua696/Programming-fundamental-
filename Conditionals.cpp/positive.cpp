a7c3216af05285497e490998b7edfe947b2c111e
Aiman Siddiqua <siddiquaaimen@gmail.com>
2026-03-10 14:44:21 +0500

    Check whether the number is positive or negative

---
+++ Conditionals.cpp/positive.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<" Enter your number : ";
+    cin>>num;
+    if(num > 0){
+        cout<<"Number is positive";
+    }
+    else if(num < 0){
+        cout<<"Number is negative";
+    }
+    else{
+        cout<<"Number is zero";
+    }
+    return 0;
+}
